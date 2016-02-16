e4edb14c2d5a2cffb204d091d01d66b65cb7a185
Chris Carpenter <chris.carpenter@asynchrony.com>
2016-02-16 08:16:46 +0000

    listen to all nodes, post to openthrone on status update

--- rpi/relay.cpp
+++ rpi/relay.cpp
@@ -4,43 +4,38 @@
 #include <string>
 #include <unistd.h>
 #include <RF24/RF24.h>
+#include "restclient-cpp/restclient.h"
 
 using namespace std;
 
 RF24 radio(22,0);
-// Assign a unique identifier for this node, 0 or 1
-bool radioNumber = 0;
 
-// Radio pipe addresses for the 2 nodes to communicate.
-const uint8_t pipes[][6] = {"1Node","2Node"};
-
+const uint8_t pipes[][6] = {"1Node","2Node","3Node"};
 
 int main(int argc, char** argv){
     radio.begin();
     radio.setChannel(108);
     radio.openReadingPipe(1,pipes[0]);
+    radio.openReadingPipe(2,pipes[1]);
+    radio.openReadingPipe(3,pipes[2]);
     radio.startListening();
 
 	while (1)
 	{
-	    int started_waiting_at = millis();
-        bool timeout = false;
-        while ( ! radio.available() && ! timeout ) {
-            if (millis() - started_waiting_at > 200 )
-                timeout = true;
+	    uint8_t stallNum;
+        while (!radio.available(&stallNum)) {
+            sleep(1);
         }
+        bool occupied;
 
-        if ( timeout )
-        {
-            printf("Failed, response timed out.\n");
-        }
-        else
-        {
-            int data;
-            radio.read( &data, sizeof(int) );
-            printf("Got response %i\n", data);
-        }
-        sleep(1);
+        radio.read( &occupied, sizeof(bool) );
+        printf("Got response %s from stall %i \n", occupied ? "occupied" : "empty", stallNum);
+
+        ostringstream json, address;
+        json << "{\"occupied\": \"" << occupied << "\"}";
+        address << "http://openthrone.com/stall/" << (int)stallNum;
+        RestClient::Response r = RestClient::post(address.str(), "text/json", json.str());
+        cout << r.code << "\n";
 	}
 
   return 0;
