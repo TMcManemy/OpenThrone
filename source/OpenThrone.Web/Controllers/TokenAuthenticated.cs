0348e20d89fff5eb1842f86fe8d9dc26482abad9
Tim McManemy <tim.mcmanemy@gmail.com>
2016-02-26 08:53:05 -0600

    Rough first go at token-based authentication

---
+++ source/OpenThrone.Web/Controllers/TokenAuthenticated.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace OpenThrone.Web.Controllers
+{
+    public class TokenAuthenticated : Attribute, IAuthenticationFilter
+    {
+        public bool AllowMultiple { get; private set; }
+
+        public async Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            var request = context.Request;
+            var authorization = request.Headers.Authorization;
+            if (authorization == null ||
+                authorization.Scheme != "Bearer" ||
+                String.IsNullOrWhiteSpace(authorization.Parameter)) return;
+
+            //validation
+            var token = authorization.Parameter;
+            if (token != "A") return;
+
+            var identity = new ClaimsIdentity(new[] {new Claim("Token", token)}, "ExternalBearer");
+            context.Principal = new ClaimsPrincipal(identity);
+        }
+
+        public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+        {
+            var challenge = new AuthenticationHeaderValue("Bearer");
+            context.Result = new AddChallengeOnUnauthorizedResult(challenge, context.Result);
+            return Task.FromResult(0);
+        }
+
+        private class AddChallengeOnUnauthorizedResult : IHttpActionResult
+        {
+            private readonly AuthenticationHeaderValue _challenge;
+            private readonly IHttpActionResult _innerResult;
+
+            public AddChallengeOnUnauthorizedResult(AuthenticationHeaderValue challenge, IHttpActionResult innerResult)
+            {
+                _challenge = challenge;
+                _innerResult = innerResult;
+            }
+
+            public async Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+            {
+                var response = await _innerResult.ExecuteAsync(cancellationToken);
+
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    //Only add one challenge per authentication scheme.
+                    if (!response.Headers.WwwAuthenticate.Any((h) => h.Scheme == _challenge.Scheme))
+                    {
+                        response.Headers.WwwAuthenticate.Add(_challenge);
+                    }
+                }
+
+                return response;
+            }
+        }
+    }
+}
