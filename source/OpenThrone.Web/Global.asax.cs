ed9cb759bac77771e610a96d0d9d1bebf2e1d214
Tim McManemy <tim.mcmanemy@gmail.com>
2016-03-02 08:40:24 -0600

    Remove unwanted IIS response headers

--- source/OpenThrone.Web/Global.asax.cs
+++ source/OpenThrone.Web/Global.asax.cs
@@ -1,15 +1,28 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace OpenThrone.Web
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            MvcHandler.DisableMvcResponseHeader = true;
             StallCache.Initialize();
+        }
+
+        protected void Application_PreSendRequestHeaders(Object source, EventArgs e)
+        {
+            var app = source as HttpApplication;
+            if (app == null) return;
+
+            var headers = app.Context.Response.Headers;
+            headers.Remove("Server");
         }
     }
 }
