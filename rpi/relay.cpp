4cf8c0cc8acdb18b4346897f9929ee267e32ba4b
Chris Carpenter <chris@uberchris.com>
2016-03-03 19:45:51 -0600

    whitespace

--- rpi/relay.cpp
+++ rpi/relay.cpp
@@ -13,31 +13,31 @@ RF24 radio(22,0);
 const uint8_t pipes[][6] = {"1Node","2Node","3Node"};
 
 int main(int argc, char** argv){
-    radio.begin();
-    radio.setChannel(108);
-    radio.openReadingPipe(1,pipes[0]);
-    radio.openReadingPipe(2,pipes[1]);
-    radio.openReadingPipe(3,pipes[2]);
-    radio.startListening();
+	radio.begin();
+	radio.setChannel(108);
+	radio.openReadingPipe(1,pipes[0]);
+	radio.openReadingPipe(2,pipes[1]);
+	radio.openReadingPipe(3,pipes[2]);
+	radio.startListening();
 
 	while (1)
 	{
-	    uint8_t stallNum;
-        while (!radio.available(&stallNum)) {
-            sleep(1);
-        }
-        bool occupied;
-
-        radio.read( &occupied, sizeof(bool) );
-        printf("Got response %s from stall %i \n", occupied ? "occupied" : "empty", stallNum);
-
-        ostringstream json, address;
-        json << "{\"available\": \"" << !occupied << "\"}";
-        address << "http://openthrone.com/api/stall/" << (int)stallNum;
-        RestClient::Response r = RestClient::put(address.str(), "text/json", json.str());
-        cout << r.code << "\n";
+		uint8_t stallNum;
+		while (!radio.available(&stallNum)) {
+			sleep(1);
+		}
+		bool occupied;
+		
+		radio.read( &occupied, sizeof(bool) );
+		printf("Got response %s from stall %i \n", occupied ? "occupied" : "empty", stallNum);
+		
+		ostringstream json, address;
+		json << "{\"available\": \"" << !occupied << "\"}";
+		address << "http://openthrone.com/api/stall/" << (int)stallNum;
+		RestClient::Response r = RestClient::put(address.str(), "text/json", json.str());
+		cout << r.code << "\n";
 	}
 
-  return 0;
+	return 0;
 }
 
